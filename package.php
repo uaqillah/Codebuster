5b3567b39e85d3f3da284d4fedc0c424ffa42168
aidatulkhairina <117662841+aidatulkhairina@users.noreply.github.com>
2022-12-14 01:55:28 +0800

    Update package.php

--- package.php
+++ package.php
@@ -1,7 +1,8 @@
 <?php 
 session_start();
+include "db_conn.php";
 
-if (isset($_SESSION['id']) && isset($_SESSION['user_name'])) {
+if (isset($_SESSION['id']) && isset($_SESSION['email'])) {
 
  ?>
 
@@ -10,7 +11,6 @@ if (isset($_SESSION['id']) && isset($_SESSION['user_name'])) {
     <head>
     <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>HOME</title>
 
 
      <style>
@@ -179,6 +179,7 @@ color: grey;
   <li><a href="explore.php">Explore</a></li>
   <li><a href="package.php">Package</a></li>
   <li><a href="contact.php">Contact</a></li>
+  
   <li style="float:right" class="dropdown">
   <a href="javascript:void(0)" class="dropbtn">Setting</a>
   <div class="dropdown-content">
@@ -186,36 +187,44 @@ color: grey;
   <a href="logout.php">Logout</a>
   </div>
 </li>
+<li style="float: right; ">
+    <form action="search.php" method="GET">
+		<input style="width:100px; height: 10px;" type="text" name="query" placeholder="Search.." />
+	</form>
+  </li>
 </ul>
+
 
 <h1 style="text-align: center;">Packages</h1>
 
 <div class="booking">
-  <div style="margin-left: 30px;">
-                                    <select class="" style="height: 47px;">
-                                        <option selected>Destination</option>
-                                        <option value="1">Sunset Cruise</option>
-                                        <option value="2">Island Hoping</option>
-                                        <option value="3">Mangrove Tour</option>
-                                    </select>
-                                </div>
-                                <div style="font-size:15px; margin-top:30px; margin-right:0px">Date: </div>
-                                <div  style="margin-left:0px;">
- <input type="date" id="date" name="date">
- </div>
-  <div>
-  <label style="font-size:15px" for="adult">Adult:</label>
- <input placeholder="0" style="width: 80px" type="text" id="adult" name="adult">
-  </div> 
-  <div>
-  <label style="font-size:15px" for="children">Children:</label>
- <input placeholder="0" style="width: 80px" type="text" id="children" name="children">
-  </div> 
-  <div style="margin-left: 50px;">
-  <input action="/package.php" type="submit" value="Book">
-  </div>
+  </div><form action="book.php">
+  <div style="margin-left: 50px;width: 100px">
+  <input type="submit" value="Book">
+  </div></form>
+
   
 </div>
+
+</br>
+
+<form style="float:left; margin-left:50px" action="filter.php" method="GET">
+<div>
+<div>
+<label for="">Start Price</label>
+<input style="width:100px;" type="text" name="start_price" value="<?php if(isset($_GET['start_price'])){echo $_GET['start_price']; }else{echo "0";} ?>">
+</div>
+<div>
+<label>End Price</label>
+<input style="width:100px;" type="text" name="end_price" value="<?php if(isset($_GET['end_price'])){echo $_GET['end_price']; }else{echo "250";} ?>">
+</div>
+<div>
+<button type="submit">Filter</button>
+</div>
+</div>
+</form>
+</br></br></br></br></br></br></br>
+
 <form style="float: right;" action="fav.php" method="post" style="width: 30%;">
   <input 
 style="float: right;margin-top:0px;width:90px;padding:0px;margin-right:0px;"
@@ -235,7 +244,7 @@ type="submit" name="submit" Value="My Favourite"/>
 <div class="container" style="margin-top: 50px;margin-left:50px;">
   <div style="flex-basis:300px"><img style="width: 100%; height: 100%" src="https://i.pinimg.com/564x/20/e6/70/20e670765ec0793e5ef8e296402430f5.jpg"></div>
   <div style="flex-basis:500px; ">
-  <p style="margin-top: 20px;"><b>Sunset Cruise</b> <br/><br/> Great for travelling couples and honeymooners, it also includes buffet dinner on cruise <br/><br/><br/> <a style="color:white" href="sunset.php">Read more</a></p>
+  <p style="margin-top: 20px;"><b>Sunset Cruise (RM200)</b> <br/><br/> Great for travelling couples and honeymooners, it also includes buffet dinner on cruise <br/><br/><br/> <a style="color:white" href="sunset.php">Read more</a></p>
   </div>
  
 </div>
@@ -243,7 +252,7 @@ type="submit" name="submit" Value="My Favourite"/>
 <div class="container" style="margin-top: 50px;margin-left:50px;">
   <div style="flex-basis:300px"><img style="width: 100%; height: 100%" src="https://www.langkawi-insight.com/langkawi_i00001e.jpg"></div>
   <div style="flex-basis:500px; ">
-  <p style="margin-top: 20px;"><b>Mangrove Tour</b> <br/><br/> 
+  <p style="margin-top: 20px;"><b>Mangrove Tour (RM120)</b> <br/><br/> 
   Offers some truly unforgettable sights and experiences on this tour while passing through
   winding river, you will observe the wonders of the park's marine ecosystem, its flora and fauna and the natural inhabitants
   <br/><br/><br/> <a style="color:white" href="mangrove.php">Read more</a></p>
@@ -253,7 +262,7 @@ type="submit" name="submit" Value="My Favourite"/>
 <div class="container" style="margin-top: 50px;margin-left:50px;">
   <div style="flex-basis:300px"><img style="width: 100%; height: 100%" src="https://tourslangkawi.com/wp-content/uploads/2021/10/Island-Hopping-Langkawi-04-1-300x300.jpg"></div>
   <div style="flex-basis:500px; ">
-  <p style="margin-top: 20px;"><b>Island Hoping</b> <br/><br/> 
+  <p style="margin-top: 20px;"><b>Island Hoping (RM50)</b> <br/><br/> 
   Visit Langkawi's popular islands on a boat tour, perfect for nature lovers
     and also witness magnificent eagles
   <br/><br/><br/> <br/><br/><br/> <br/><a style="color:white" href="hoping.php">Read more</a></p>
